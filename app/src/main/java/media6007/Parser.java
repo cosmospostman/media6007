9fa227da2e749f21a991f4ee1ae8f2961dfe2826
cosmospostman <buck68@gmail.com>
2021-07-11 22:15:14 +1000

    Gradle

---
+++ app/src/main/java/media6007/Parser.java
@@ -0,0 +1,10 @@
+package media6007;
+
+public class Parser {
+    
+    final static String DATA_PATH = "data/";
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
